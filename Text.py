f666d9b64619e13208113d75135cb287eb4febca
Goldmyn <77717543+Goldmyn@users.noreply.github.com>
2021-07-24 01:12:41 +0100

    Code.py
    A sample python code for a Number Guessing Game.

---
+++ Text.py
@@ -0,0 +1,28 @@
+#start by importing random.
+from random import randint
+
+#creating a function
+
+def gameon():
+    while True:
+        x = randint(1,20)
+        u = input('Select a number from 1-20 :\n')
+#a try statement against using characters.
+        try:
+            userx = int(u)
+        except:
+            print('Not a number.')
+            print('Try again')
+            continue
+        if userx == x:
+            print('Congratulations\n You Won!!')
+            break
+        else:
+            print('Ooo Noo\n Wrong choice.')
+            print('Try Again')
+
+#A welcome statement.
+print('Welcome Player\n Select any number from 1 - 20 to Play.')
+
+#calling the above function.
+gameon()
